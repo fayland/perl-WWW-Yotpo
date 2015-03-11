9739fa20f77e2d872f5bf98ea130ca68f5e61898
Fayland Lam <fayland@gmail.com>
2015-03-11 15:49:32 +0800

    Initial commit

---
+++ lib/WWW/Yotpo.pm
@@ -0,0 +1,39 @@
+package WWW::Yotpo;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::Yotpo - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use WWW::Yotpo;
+
+=head1 DESCRIPTION
+
+WWW::Yotpo is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@binary.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
